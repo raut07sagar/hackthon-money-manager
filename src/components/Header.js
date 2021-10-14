5c696770c90196ae7ec471a0edc7de4316b0b18c
raut07sagar <rautsagar622@gmail.com>
2021-10-14 11:33:20 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className="img00">
+    <h2 className="moneyhead">
+      Money Manager App
+    </h2>
+    <img height="90" src="https://media3.giphy.com/media/UU191hoOMwwskvOQZZ/giphy.webp" alt="" />
+    </div> 
+  )
+}
